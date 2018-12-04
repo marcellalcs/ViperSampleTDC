49066f9abf9e492455d51c8e4efb7a15c79b3758
marcella souza <mlsouza@thoughtworks.com>
2018-12-04 11:04:54 -0200

    step-1 - Adjusting entity to new API

--- app/src/main/java/linearlayout/android/com/vipersampletdc/entity/phraseEntity.kt
+++ app/src/main/java/linearlayout/android/com/vipersampletdc/entity/phraseEntity.kt
@@ -2,4 +2,6 @@ package linearlayout.android.com.vipersampletdc.entity
 
 import com.google.gson.annotations.SerializedName
 
-data class PhraseEntity(@SerializedName("quote") var phrase: String, @SerializedName("author") var author: String)
+data class PhraseEntity(@SerializedName("quote") var phrase: String,
+                        @SerializedName("author") var author: String,
+                        @SerializedName("cat") var category: String)
