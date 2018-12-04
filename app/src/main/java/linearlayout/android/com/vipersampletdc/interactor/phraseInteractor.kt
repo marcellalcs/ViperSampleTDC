e507f0f5580d8d25f899017ee72d4aaf7a442719
marcella souza <mlsouza@thoughtworks.com>
2018-12-04 11:07:32 -0200

    step-2 - Adjusting interactor to API

--- app/src/main/java/linearlayout/android/com/vipersampletdc/interactor/phraseInteractor.kt
+++ app/src/main/java/linearlayout/android/com/vipersampletdc/interactor/phraseInteractor.kt
@@ -3,14 +3,14 @@ package linearlayout.android.com.vipersampletdc.interactor
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import linearlayout.android.com.vipersampletdc.entity.PhraseEntity
 import linearlayout.android.com.vipersampletdc.interactor.utils.HTTPManager
 import linearlayout.android.com.vipersampletdc.interactor.utils.getRandomElement
 
 class PhraseInteractor {
 
-    fun fetchPhrases(): Observable<String?> {
+    fun fetchPhrase(): Observable<PhraseEntity> {
         return HTTPManager.phrasesService.fetchRandomPhrase()
-                .map { it.phrases.getRandomElement()?.phrase }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
     }
